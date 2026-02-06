791dde272b53be358811dc732f2fba685714ec82
Anna Kuts <kuutsann@gmail.com>
2026-02-06 12:58:42 +0200

    refactor: rename messageBox variables for consistency

--- lab1/1-10-IM-41-Kuts-32.asm
+++ lab1/1-10-IM-41-Kuts-32.asm
@@ -1,5 +1,6 @@
 .386
 .model flat, stdcall
+
 option casemap:none
 
 include \masm32\include\windows.inc
@@ -10,7 +11,7 @@ includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\user32.lib
 
 .data
-messageBoxContent db "ПІБ: Куц Анна Василівна",13,10
+msgText db "ПІБ: Куц Анна Василівна",13,10
 db "Дата народження: 03.12.2007",13,10
 db "Улюблена виконавиця: Sabrina Carpenter",13,10
 db "Улюблена пісня: Espresso",13,10
@@ -24,10 +25,10 @@ db "Любов Степанівну ПРИБИЛО",13,10
 db "Було дуже весело виконувати лабу <3",13,10
 db "Як гадаєте, достатньо особистої інформації?",0
 
-messageBoxTitle db "Лабораторна 1",0
+msgTitle db "Лабораторна 1",0
 
 .code
-start:
-invoke MessageBoxA, NULL, addr messageBoxContent, addr messageBoxTitle, MB_YESNO or MB_ICONQUESTION
+main:
+invoke MessageBoxA, NULL, addr msgText, addr msgTitle, MB_YESNO or MB_ICONQUESTION
 invoke ExitProcess, 0
-end start
+end main

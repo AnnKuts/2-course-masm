d81b917e5d6cc9bd244a5dcf427435155aa18033
Ann Kuts <kuutsann@gmail.com>
2026-02-07 19:24:36 +0200

    feat: change masm64 fiel

--- lab1/1-10-IM-41-Kuts-64.asm
+++ lab1/1-10-IM-41-Kuts-64.asm
@@ -1,14 +1,21 @@
 OPTION DOTNAME
 option casemap:none
 
-extern MessageBoxA:proc
-extern ExitProcess:proc
+include \masm64\include\temphls.inc
+include \masm64\include\win64.inc
+include \masm64\include\kernel32.inc
+include \masm64\include\user32.inc
+includelib \masm64\lib\kernel32.lib
+includelib \masm64\lib\user32.lib
 
 MB_YESNO equ 4
 MB_ICONQUESTION equ 32
 
+OPTION PROLOGUE:rbpFramePrologue
+OPTION EPILOGUE:none
+
 .data
-msgText db "ПІБ: Куц Анна Василівна",13,10
+msgText db "ПІБ: Куц Анна Василівна", 13, 10
 db "Дата народження: 03.12.2007",13,10
 db "Улюблена виконавиця: Sabrina Carpenter",13,10
 db "Улюблена пісня: Espresso",13,10
@@ -21,18 +28,13 @@ db "і надзвичайно щиру",13,10
 db "Любов Степанівну ПРИБИЛО",13,10
 db "Було дуже весело виконувати лабу <3",13,10
 db "Як гадаєте, достатньо особистої інформації?",0
-msgTitle db "Лабораторна 1",0
+msgTitle db "Лабораторна 1", 0
 
 .code
-main proc
+main proc 
 sub rsp, 28h
-xor rcx, rcx
-lea rdx, msgText
-lea r8,  msgTitle
-mov r9d, MB_YESNO or MB_ICONQUESTION
-call MessageBoxA
-xor ecx, ecx
-call ExitProcess
-
+invoke MessageBox, NULL, addr msgText, addr msgTitle, MB_YESNO or MB_ICONQUESTION
+invoke ExitProcess, NULL
 main endp
+
 end
